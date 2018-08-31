2dfc680d3a177cfd7d17363450ed3452745db999
Khalifa Esha Iftitah <Khalifa_EI326@mitrais.com>
2018-08-31 10:04:04 +0700

    #add user table

---
+++ db/rmsdb_user.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.14 
+MySQL - 5.5.5-10.1.32-MariaDB : Database - rmsdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rmsdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `rmsdb`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`user_name`,`password`) values (1,'Mandra','123456'),(2,'Cimonah','123456'),(3,'Cimohaha','123456'),(4,'Brian','123456'),(6,'tetew','123456'),(7,'bono','654321');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
